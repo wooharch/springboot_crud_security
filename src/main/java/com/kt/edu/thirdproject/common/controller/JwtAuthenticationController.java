293078d200cd422d6076d4bdf7bc8bec3705f4d7
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-09-13 23:03:46 +0900

    Update JwtAuthenticationController.java

--- src/main/java/com/kt/edu/thirdproject/common/controller/JwtAuthenticationController.java
+++ src/main/java/com/kt/edu/thirdproject/common/controller/JwtAuthenticationController.java
@@ -15,7 +15,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origin="*", allowedHeaders = "*")
 @RestController
 public class JwtAuthenticationController {
 
