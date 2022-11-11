f209ae067df3a3a5dfd5508ab32c2a03bfdac8bf
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-11-11 16:14:39 +0900

    Update QueryEmployeeSqls.java

--- src/main/java/com/kt/edu/thirdproject/employee/repository/sql/QueryEmployeeSqls.java
+++ src/main/java/com/kt/edu/thirdproject/employee/repository/sql/QueryEmployeeSqls.java
@@ -2,8 +2,10 @@ package com.kt.edu.thirdproject.employee.repository.sql;
 
 public class QueryEmployeeSqls {
 
+    // h2 db
+    //  SELECT hibernate_sequence.nextval FROM  dual; 
+
     public static final String RETV_NEXT_VAL="""
     SELECT NEXTVAL(hibernate_sequence);    
-   //  SELECT hibernate_sequence.nextval FROM  dual 
            """;
 }
