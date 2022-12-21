88a91d98f28e6c845dd11c71306795c6818e3ce2
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-12-21 15:56:15 +0900

    Update QueryEmployeeSqls.java

--- src/main/java/com/kt/edu/thirdproject/employee/repository/sql/QueryEmployeeSqls.java
+++ src/main/java/com/kt/edu/thirdproject/employee/repository/sql/QueryEmployeeSqls.java
@@ -7,20 +7,21 @@ public class QueryEmployeeSqls {
     
     private String hibernate_nextval;
     
-    @Value("${spring.profiles.active:}") 
+    @Value("${spring.profiles.active:Unknown}")
     private String activeProfile;
     
-    if activeProfile.equals("prd"){ // maria, mysql
-       hibernate_nextval = "SELECT NEXTVAL(hibernate_sequence)";
-    } else { // h2 db
-       hibernate_nextval = "SELECT hibernate_sequence.nextval FROM  dual"; 
-    }  
+    //if activeProfile.equals("prd"){ // maria, mysql
+    //   hibernate_nextval = "SELECT NEXTVAL(hibernate_sequence)";
+   // } else { // h2 db
+   //    hibernate_nextval = "SELECT hibernate_sequence.nextval FROM  dual"; 
+   // }  
     
-    /*
+    
     public static final String RETV_NEXT_VAL="""
-    SELECT NEXTVAL(hibernate_sequence);    
+    SELECT hibernate_sequence.nextval FROM  dual;    
            """;
-    */
     
-    public static final String RETV_NEXT_VAL = hibernate_nextval;
+    //     SELECT NEXTVAL(hibernate_sequence);    mariadb
+
+    //public static final String RETV_NEXT_VAL = hibernate_nextval;
 }
