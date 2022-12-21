48ca78446f8512009c479d591b3e0023f06250e7
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-12-21 15:40:48 +0900

    Update QueryEmployeeSqls.java

--- src/main/java/com/kt/edu/thirdproject/employee/repository/sql/QueryEmployeeSqls.java
+++ src/main/java/com/kt/edu/thirdproject/employee/repository/sql/QueryEmployeeSqls.java
@@ -4,8 +4,22 @@ public class QueryEmployeeSqls {
 
     // h2 db
     //  SELECT hibernate_sequence.nextval FROM  dual; 
-
+    
+    private String hibernate_nextval;
+    
+    @Value("${spring.profiles.active:}") 
+    private String activeProfile;
+    if activeProfile.equals("prd"){ // maria, mysql
+       hibernate_nextval = "SELECT NEXTVAL(hibernate_sequence)";
+    } else { // h2 db
+       hibernate_nextval = "SELECT hibernate_sequence.nextval FROM  dual"; 
+    }  
+    
+    /*
     public static final String RETV_NEXT_VAL="""
     SELECT NEXTVAL(hibernate_sequence);    
            """;
+    */
+    
+    public static final String RETV_NEXT_VAL=hibernate_nextval;
 }
