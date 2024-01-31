20b448cba89408b47a5063f9fa4822737c059436
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-01-31 15:38:53 +0900

    Update EmployeeController.java

--- src/main/java/com/kt/edu/thirdproject/employee/controller/EmployeeController.java
+++ src/main/java/com/kt/edu/thirdproject/employee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.kt.edu.thirdproject.employee.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,7 @@ import java.util.List;
 
 @Tag(name = "posts", description = "Employee API")
 @RestController
-// @RequiredArgsConstructor
+//@RequiredArgsConstructor
 @CrossOrigin(origins ="*")
 @RequestMapping("/api/v1/")
 public class EmployeeController {
