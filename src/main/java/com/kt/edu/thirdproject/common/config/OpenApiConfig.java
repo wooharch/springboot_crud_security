1bb575ec344f704abb6eb5628c58e81fee578a11
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-02-18 21:20:27 +0900

    Update OpenApiConfig.java

--- src/main/java/com/kt/edu/thirdproject/common/config/OpenApiConfig.java
+++ src/main/java/com/kt/edu/thirdproject/common/config/OpenApiConfig.java
@@ -1,5 +1,8 @@
 package com.kt.edu.thirdproject.common.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +16,12 @@ import io.swagger.v3.oas.models.info.License;
 @Configuration
 public class OpenApiConfig {
 
+
     @Value("${spring.application.name}")
-    private String group;
+    private String applicationName;
+
+    //@Value("${spring.application.name}")
+    //private String group;
 
     @Value("${app-info.title}")
     private String title;
@@ -37,16 +44,38 @@ public class OpenApiConfig {
     @Value("${app-info.doc-url}")
     private String docUurl;
 
-    @Profile({"local", "dev"})
+    /*@Profile({"local", "dev"})
     @Bean
     public GroupedOpenApi openApi() {
         String[] paths = {"/**"};
         return GroupedOpenApi.builder().group(group).pathsToMatch(paths).build();
+    } */
+
+    @Profile({"local", "dev"})
+    @Bean
+    public GroupedOpenApi customApi() {
+        return GroupedOpenApi.builder().group("api").pathsToMatch("/api/**").build();
+    }
+
+    @Profile({"local", "dev"})
+    @Bean
+    public GroupedOpenApi actuatorApi() {
+        return GroupedOpenApi.builder().group("actuator").pathsToMatch("/actuator/**").build();
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info().title(title)
                         .description(desc)
                         .version(version)
@@ -57,5 +86,3 @@ public class OpenApiConfig {
     }
 
 }
-
-
